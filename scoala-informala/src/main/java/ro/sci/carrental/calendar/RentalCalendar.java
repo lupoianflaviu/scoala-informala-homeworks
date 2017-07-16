301fdb6b52b0b6e6d7de5d4ed889434a060766b9
lupoianflaviu <lupoianflaviu@yahoo.com>
2017-07-16 12:29:03 +0300

    Modified use of Generics in RentalCalendar Modified use of Generics to use only one parameter T extends Type Needed and other corrections suggested by Sebi Removed the usage of Casts!
    Added customers.txt for customer initialization, initialization not performed yet
    Added Reader for Car and Customer initialization
    Correct use of Generics
    Use only Service layer in Main, and only Interface Type for declaration
    Use Logger instead of System.out and Exception in calculateRentPrice()
    
    used generics for calculating RentalPrice
    and in CarRepository and CarService
    created a test Class for RentalPrice
    please review and make suggestions!

--- scoala-informala/src/main/java/ro/sci/carrental/calendar/RentalCalendar.java
+++ scoala-informala/src/main/java/ro/sci/carrental/calendar/RentalCalendar.java
@@ -3,12 +3,12 @@ package ro.sci.carrental.calendar;
 /**
  * Interface for scheduling a rental.
  */
-public interface RentalCalendar<T, V> {
+public interface RentalCalendar<Car, Number> {
     /**
      * Method calculateRentPrice calculates the rental price of a Car for a given number o rented days.
-     * @param t Type Car representing the rented car
-     * @param v Type Number representing Renting interval
+     * @param car
+     * @param number
      * @return
      */
-    double calculateRentPrice(T t, V v);
+    double calculateRentPrice(Car car, Number number);
 }
